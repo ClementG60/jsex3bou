6c018e0d8cc72a8a1ddd2da55c3ead8d0fbfca5a
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-03 11:40:12 +0100

    modif js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
-for (let i = 0; i < lang.length; i++) {
-    console.log(languages[i])
+for (let i = 0; i < languages.length; i++) {
+    document.write(languages[i], "<br>")
 }
